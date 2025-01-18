8d397643ba45cbab0a92c4935dbc8738fc6572e1
okewunmi <okewunmiafeezolaide@gmail.com>
2025-01-19 00:33:05 +0100

    update

--- app/(tabs)/profile.js
+++ app/(tabs)/profile.js
@@ -1,14 +1,28 @@
 import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import React from "react";
 
 const profile = () => {
   return (
-    <View>
+    <SafeAreaView style={styles.safe} >
+      <View>
       <Text>library</Text>
     </View>
+    </SafeAreaView>
+    
   );
 };
 
 export default profile;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+safe: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+    backgroundColor: "#ffff",
+    paddingVertical: 25,
+    paddingHorizontal: 15,
+  },
+});
